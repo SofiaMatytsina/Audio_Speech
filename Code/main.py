6c89d1e5639e4329e387b208c15895788af6455e
SofiaMatytsina <74772375+SofiaMatytsina@users.noreply.github.com>
2022-07-19 16:36:21 +0300

    Several audios
    Worked

--- Code/main.py
+++ Code/main.py
@@ -9,28 +9,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-folder = 'C:/Users/Mi/Desktop/openSMILE/bin/Audio/Wav' 
+folder = 'C:/Users/Mi/Documents/GitHub/Audio_Speech/Code' 
 
 def handle_allAudio(): #обрботка всех аудиофайлов файлов в папке
     for audio_data in os.listdir(folder):
         if audio_data.endswith('.wav') and os.path.isfile(os.path.join(folder, audio_data)):
             def diagramPlot(): #построение спектрограмм и графиков
                 y, sr = librosa.load(audio_data)
+                audio_name = os.path.splitext(audio_data)[0]
                 print(type(y), type(sr))
                 plt.figure(figsize=(14, 5))
 
                 #Построение аудиограммы
                 librosa.display.waveshow(y, sr=sr)
                 ipd.Audio(audio_data)
+                plt.savefig(audio_name + '_Audiogram.png')
 
                 #Построение спектрограммы
                 X = librosa.stft(y)
                 Xdb = librosa.amplitude_to_db(abs(X))
                 librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
                 plt.colorbar()
-                plt.savefig(folder + audio_data + '.png')
+                plt.savefig(audio_name + '_Spectrogram.png')
 
                 #Построение мел-спектрограммы
                 librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='mel')
-                plt.colorbar()
-                plt.savefig('Note_A_3_MelSpec.png')
+                plt.savefig(audio_name + '_MelSpectrogram.png')
+
+            diagramPlot()
+
+
+if __name__ == '__main__':
+    handle_allAudio()
